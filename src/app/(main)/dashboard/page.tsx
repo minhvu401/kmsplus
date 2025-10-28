622e474d4258d49346b01e561b006c6b4610abc2
KhanhPND <duykhanh4239@gmail.com>
2025-10-28 14:40:15 +0700

    feat: big update 28/10

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import PrivateLayout from "@/components/layout/PrivateLayout"
+import GeneralInfromation from "./components/GeneralInfromation"
+
+export default function DashboardPage() {
+  return <GeneralInfromation />
+}
