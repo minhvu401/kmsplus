92038b68a3c51c549645717975dccb648132de82
minhvu401 <minhvuhoang4104@gmail.com>
2026-01-15 19:10:51 +0700

    Revert "Merge branch 'development' into dev/NhatTT"
    This reverts commit 480fd6437cc12a8e10e3e5fbfc0850585a715d5e, reversing
    changes made to df0fca3ec36c63cd6a55602ade50d0da37dcd088.

--- src/app/(main)/questions/page.tsx
+++ src/app/(main)/questions/page.tsx
@@ -2,38 +2,51 @@
 import PageWrapper from "@/components/ui/questions/page-wrapper"
 import Search from "@/components/ui/questions/search"
 import { CreateQuestion } from "@/components/ui/questions/create-button"
-import { FilterCategory, FilterStatus, QuestionsSortBy } from "@/components/ui/questions/filters"
-import { getActiveCategories, fetchQuestionsPages, fetchFilteredQuestions } from "@/action/question/questionActions";
-import QuestionsNotification from "@/components/ui/questions/questions-notification";
-import Pagination from "@/components/ui/questions/pagination";
-import QuestionsList from "@/components/ui/questions/questions-list";
-import { Flex } from "antd";
+import {
+  FilterCategory,
+  FilterStatus,
+  QuestionsSortBy,
+} from "@/components/ui/questions/filters"
+import {
+  getActiveCategories,
+  fetchQuestionsPages,
+  fetchFilteredQuestions,
+} from "@/action/question/questionActions"
+import QuestionsNotification from "@/components/ui/questions/questions-notification"
+import Pagination from "@/components/ui/questions/pagination"
+import QuestionsList from "@/components/ui/questions/questions-list"
+import { Flex } from "antd"
 
 export default async function Page(props: {
   searchParams?: Promise<{
-    query?: string;
-    page?: string;
-    category?: string;
-    status?: string;
-    sort?: string;
-  }>;
+    query?: string
+    page?: string
+    category?: string
+    status?: string
+    sort?: string
+  }>
 }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const category = searchParams?.category || 'any';
-  const status = searchParams?.status || 'any';
-  const sort = searchParams?.sort || 'newest';
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchQuestionsPages(query, category, status);
+  const searchParams = await props.searchParams
+  const query = searchParams?.query || ""
+  const category = searchParams?.category || "any"
+  const status = searchParams?.status || "any"
+  const sort = searchParams?.sort || "newest"
+  const currentPage = Number(searchParams?.page) || 1
+  const totalPages = await fetchQuestionsPages(query, category, status)
 
-  const categories = await getActiveCategories();
-  const questions = await fetchFilteredQuestions(query, category, status, sort, currentPage);
+  const categories = await getActiveCategories()
+  const questions = await fetchFilteredQuestions(
+    query,
+    category,
+    status,
+    sort,
+    currentPage
+  )
 
-  const noSearchResults = query !== '' && questions.length === 0;
+  const noSearchResults = query !== "" && questions.length === 0
 
   return (
     <PageWrapper>
-
       <QuestionsNotification />
 
       <Flex align="center" gap={28} style={{ marginBottom: 24 }}>
@@ -47,12 +60,16 @@ export default async function Page(props: {
         <QuestionsSortBy />
       </Flex>
 
-      <div style={{ marginBottom: 24 }}>
-        <QuestionsList questions={questions} noSearchResults={noSearchResults} />
-      </div>
-      <div className="flex justify-end items-center gap-14 mb-6">
+      <Flex style={{ marginBottom: 24 }}>
+        <QuestionsList
+          questions={questions}
+          noSearchResults={noSearchResults}
+        />
+      </Flex>
+
+      <Flex justify="end" align="center" style={{ marginBottom: 24 }}>
         <Pagination totalPages={totalPages} />
       </Flex>
     </PageWrapper>
-  );
+  )
 }
