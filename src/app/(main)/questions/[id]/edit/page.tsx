de76d73f9675e3e0d03fe5878fc9417da4358ee8
minhvu401 <minhvuhoang4104@gmail.com>
2026-01-15 19:03:10 +0700

    Revert "Merge branch 'development' into dev/NhatTT"
    This reverts commit 480fd6437cc12a8e10e3e5fbfc0850585a715d5e, reversing
    changes made to df0fca3ec36c63cd6a55602ade50d0da37dcd088.

--- src/app/(main)/questions/[id]/edit/page.tsx
+++ src/app/(main)/questions/[id]/edit/page.tsx
@@ -1,5 +1,27 @@
-"use client"
+import PageWrapper from "@/components/ui/questions/page-wrapper"
+import UpdateQuestionForm from "@/components/forms/update-question-form";
+import { getActiveCategories } from "@/action/question/questionActions";
+import { getQuestionDetails } from "@/action/question/questionActions";
+import { Flex } from "antd";
 
-export default function Page() {
-  return <div>Hello!</div>
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const id = params.id;
+  const currentData = await getQuestionDetails(id);
+  const categories = await getActiveCategories();
+
+
+  return (
+    <PageWrapper>
+      <Flex align="center" gap={56}>
+        <UpdateQuestionForm
+          categories={categories}
+          currentData={currentData}
+        />
+      </Flex>
+    </PageWrapper>
+  )
 }
